2d3e6496494305341d9ccf3d8d775f146e44dd99
munindra-ai <manindratamang5@gmail.com>
2025-03-17 10:12:50 +0545

    :zap: Implement service provider registration for Debugbar and Backup packages

--- src/LaravelUsefulPackages.php
+++ src/LaravelUsefulPackages.php
@@ -2,4 +2,21 @@
 
 namespace Munindraai\LaravelUsefulPackages;
 
-class LaravelUsefulPackages {}
+use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\App;
+use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Spatie\Backup\BackupServiceProvider;
+
+class LaravelUsefulPackages extends ServiceProvider
+{
+    public function register()
+    {
+        if (!App::providerIsLoaded(DebugbarServiceProvider::class)) {
+            $this->app->register(DebugbarServiceProvider::class);
+        }
+
+        if (!App::resolved(BackupServiceProvider::class)) {
+            $this->app->register(BackupServiceProvider::class);
+        }
+    }
+}
