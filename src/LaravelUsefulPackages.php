61e02b6c7fbaf4d8195d25ae008faef5a40643da
munindra-ai <manindratamang5@gmail.com>
2025-03-17 14:41:16 +0545

    :package: Add Spatie Media Library package and register service provider

--- src/LaravelUsefulPackages.php
+++ src/LaravelUsefulPackages.php
@@ -5,7 +5,9 @@ namespace Munindraai\LaravelUsefulPackages;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\App;
 use Barryvdh\Debugbar\ServiceProvider as DebugbarServiceProvider;
+use Illuminate\Support\Facades\Artisan;
 use Spatie\Backup\BackupServiceProvider;
+use Spatie\MediaLibrary\MediaLibraryServiceProvider;
 
 class LaravelUsefulPackages extends ServiceProvider
 {
@@ -17,6 +19,25 @@ class LaravelUsefulPackages extends ServiceProvider
 
         if (!App::resolved(BackupServiceProvider::class)) {
             $this->app->register(BackupServiceProvider::class);
+        }
+        if (!App::resolved(MediaLibraryServiceProvider::class)) {
+            $this->app->register(MediaLibraryServiceProvider::class);
+        }
+    }
+
+    public function boot()
+    {
+        // Automatically publish config files if they don't exist
+        if (!file_exists(config_path('backup.php'))) {
+            Artisan::call('vendor:publish', [
+                '--provider' => "Spatie\\Backup\\BackupServiceProvider"
+            ]);
+        }
+
+        if (!file_exists(config_path('medialibrary.php'))) {
+            Artisan::call('vendor:publish', [
+                '--provider' => "Spatie\\MediaLibrary\\MediaLibraryServiceProvider"
+            ]);
         }
     }
 }
