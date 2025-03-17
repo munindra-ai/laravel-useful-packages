3468f0e913a2fe1e9fa3712bb4685ce916f4e775
munindra-ai <manindratamang5@gmail.com>
2025-03-17 15:34:42 +0545

    :refactor: Move vendor publish commands to composer.json and remove redundant boot method

--- src/LaravelUsefulPackages.php
+++ src/LaravelUsefulPackages.php
@@ -25,20 +25,4 @@ class LaravelUsefulPackages extends ServiceProvider
             $this->app->register(MediaLibraryServiceProvider::class);
         }
     }
-
-    public function boot()
-    {
-        if (!File::exists(config_path('backup.php'))) {
-            $this->publishConfig(BackupServiceProvider::class);
-        }
-
-        if (!File::exists(config_path('medialibrary.php'))) {
-            $this->publishConfig(MediaLibraryServiceProvider::class);
-        }
-    }
-
-    protected function publishConfig(string $provider)
-    {
-        exec("php artisan vendor:publish --provider=\"$provider\"");
-    }
 }
